6f9231633b36c9cb06b5d8aad610a852699fb68c
hm <1134048328@qq.com>
2017-03-29 19:34:48 +0800

    Array

--- JavaBase/src/com/array/Array.java
+++ JavaBase/src/com/array/Array.java
@@ -1,16 +1,45 @@
 package com.array;
 
+import static java.lang.System.out;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array
 {
-
 	public static void main(String[] args)
 	{
-		int[] score ={60,70,80,90,95};
-		int sum=0;
-		double avg;
+		int[] score;
+		score=new int[10];
+		Scanner input=new Scanner(System.in);
+		System.out.println("请输入数组的十个元素：");
+		
 		for(int i=0;i<score.length;i++){
-			sum=sum+score[i];
+			score[i]=input.nextInt();
 		}
-		System.out.println("输出平均数："+(avg=sum/score.length));
+		
+		System.out.println("请输入十个数：");
+		for(int i=0;i<score.length;i++){
+		System.out.println("score[" +i +"] = " + score[i] + ";");
 	}
+		int sum=0;
+		for(int i=0;i<score.length;i++){
+			sum+=score[i];
+		}
+			System.out.println("avg="+(sum/score.length));
+	
+		
+		
+		Arrays.sort(score);
+		System.out.println("升序后逐个循环输出：");
+		for (int i = 0; i < score.length; i++) {
+			System.out.print( score[i] + " ");
+		}
+		System.out.println(" ");
+		Arrays.sort(score);
+		System.out.println("降序后逐个循环输出：");
+		for (int i = 9; i < score.length&&i>=0; i--) {
+			System.out.print( score[i] + " ");
+		}
+}
 }
