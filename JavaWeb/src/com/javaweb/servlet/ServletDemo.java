fed6e3c1b62afcceab72dd361c57afa322f1fbde
hm <1134048328@qq.com>
2017-05-15 19:30:07 +0800

    servlet

---
+++ JavaWeb/src/com/javaweb/servlet/ServletDemo.java
@@ -0,0 +1,53 @@
+package com.javaweb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDemo
+ */
+@WebServlet("/servlet/ServletDemo")
+public class ServletDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDemo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.setContentType("text/html;Charset=UTF-8");//设置生成文档类型
+		PrintWriter out=response.getWriter();//得到输出字符输出流
+		out.println("<HTML>");//输出相应的HTML源文件
+		out.println("<body>");
+		out.println("<h2>我们正在学习Servlet编程</h2>");
+		out.println("<h3>现在的时间是:"+new Date()+"</h3>");//输出系统时间
+		out.println("</HTML>");
+		out.println("</body>");
+		out.close();//关闭输出流 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
