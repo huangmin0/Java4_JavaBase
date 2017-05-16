173e364f830e0c1831fb576cf21e80710b772438
hm <1134048328@qq.com>
2017-05-16 19:31:27 +0800

    servlet

---
+++ JavaWeb/src/com/javaweb/servlet/ChineseEncoding.java
@@ -0,0 +1,68 @@
+package com.javaweb.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ChineseEncoding
+ */
+@WebServlet("/servlet/ChineseEncoding")
+public class ChineseEncoding extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ChineseEncoding() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		out.println("<HTML>");
+		out.println("<head><title>解决中文乱码问题</title></head>");
+		out.println("<body>");
+		out.println("<h3>调用了doGet()方法</h3><p>");
+		out.println("<font size=4>用户输入信息:</font>");
+		String info=request.getParameter("information");
+		String newinfo=new String(info.getBytes("iso8859_1"),"UTF-8");
+		out.println("<h3>"+newinfo +"<h3>");
+		out.println("</body>");
+		out.println("</HTML>");
+		out.close();
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		out.println("<HTML>");
+		out.println("<head><title>解决中文乱码问题</title></head>");
+		out.println("<body>");
+		out.println("<h3>调用了doPost()方法</h3><p>");
+		out.println("<font size=4>用户输入信息:</font>");
+		
+		request.setCharacterEncoding("UTF-8");//设置请求对象的字符集
+		String info=request.getParameter("information");
+		out.println("<h3>"+info +"<h3>");
+		out.println("</body>");
+		out.println("</HTML>");
+		out.close();
+	
+	}
+
+}
--- JavaWeb/src/com/javaweb/servlet/OutputInfo.java
+++ JavaWeb/src/com/javaweb/servlet/OutputInfo.java
@@ -2,6 +2,7 @@ package com.javaweb.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,33 +13,40 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class OutputInfo
  */
-@WebServlet("/servlet/OutputInfo")
+@WebServlet( "/servlet/OutputInfo")
 public class OutputInfo extends HttpServlet {
+	
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public OutputInfo() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * 
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out=response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>输出报头有关信息</TITLE></HEAD>");
+		out.println("<BODY>");
+		Enumeration<String> headers=request.getHeaderNames();
+		while(headers.hasMoreElements()){
+			String name=(String)headers.nextElement();
+			String value=request.getHeader(name);
+			out.println("<h3>"+name+":"+value+"</h3>");
+		}
+		out.println("<h3>客户端的IP地址:"+request.getRemoteAddr()+"</h3>");
+		out.println("<h3>客户端的端口号:"+request.getRemotePort()+"</h3>");
+		out.println("<h3>服务器的IP地址:"+request.getLocalAddr()+"</h3>");
+		out.println("<h3>服务器的端口号:"+request.getLocalPort()+"</h3>");
+		out.println("</BODY>");
+		out.println("</HTML>");
+		out.close();
 		
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
